fe078a156f68d01232811cdc11e6b6bcf3c44ed9
Jagadeesh Musali <jagadeesh.musali@aa.com>
2022-02-02 23:56:17 -0700

    api-users

--- apps/portfolio-api/src/main.ts
+++ apps/portfolio-api/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import express = require("express");
+import bodyParser = require("body-parser");
+import {router} from "@portfolio/data-access-users";
+const port = process.env.PORT;
+
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/users", router);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
