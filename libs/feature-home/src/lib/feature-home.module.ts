8bed6ed64efbcae81ce61f223d08c88544bbb498
Jagadeesh Musali <jagadeesh.musali@aa.com>
2022-02-13 10:08:29 -0700

    intro component

--- libs/feature-home/src/lib/feature-home.module.ts
+++ libs/feature-home/src/lib/feature-home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import {PortfolioDataAccessUserModule, reducer, profileFeatureKey} from "@portfolio/portfolio/data-access-user";
+import { IntroComponent } from './components/intro/intro.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   imports: [
     CommonModule,
     PortfolioDataAccessUserModule,
   ],
   declarations: [
-    HomeComponent
+    HomeComponent,
+    IntroComponent,
+    HeaderComponent
   ],
 })
 export class FeatureHomeModule {}
